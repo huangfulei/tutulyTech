8a2fc1400817a12a50ad2829be5fc255c5b0bddd
Fulei Huang <fuleihuang@microsoft.com>
2021-11-12 00:40:52 +0000

    added folder structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,22 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import { SplitWithImage } from "../components/features/SplitWithImage";
+import { SplitWithImage } from "../common/components/features/SplitWithImage";
 import { Box, Flex, Text, VStack } from "@chakra-ui/layout";
-import LargeWithAppLinksAndSocial from "../components/footers/LargeWithAppLinksAndSocial";
-import LargeWithLogoLeft from "./../components/footers/LargeWithLogoLeft";
-import LargeWithNewsletter from "./../components/footers/LargeWithNewsletter";
-import LargeWithLogoCentered from "./../components/footers/LargeWithLogoCentered";
-import SimpleThreeColumns from "./../components/features/SimpleThreeColumns";
-import GridListWithHeading from "./../components/features/GridListWithHeading";
-import GridBlurredBackdrop from "./../components/testimonials/GridBlurredBackdrop";
-import WithSpeechBubbles from "./../components/testimonials/WithSpeechBubbles";
-import ThreeTierPricing from "./../components/pricing/ThreeTierPricing";
-import BasicStatistics from "./../components/statistics/BasicStatistics";
-import StatisticsWithIcon from "./../components/statistics/StatisticsWithIcon";
-import Carousel from "./../components/carousels/Carousel";
-import WithSubNavigation from "./../components/navbars/WithSubNavigation";
-import SidebarWithHeader from "./../components/navbars/SidebarWithHeader";
-import Contact from "./../components/contact/Contact";
+import LargeWithAppLinksAndSocial from "../common/components/footers/LargeWithAppLinksAndSocial";
+import LargeWithLogoLeft from "../common/components/footers/LargeWithLogoLeft";
+import LargeWithNewsletter from "../common/components/footers/LargeWithNewsletter";
+import LargeWithLogoCentered from "../common/components/footers/LargeWithLogoCentered";
+import SimpleThreeColumns from "../common/components/features/SimpleThreeColumns";
+import GridListWithHeading from "../common/components/features/GridListWithHeading";
+import GridBlurredBackdrop from "../common/components/testimonials/GridBlurredBackdrop";
+import WithSpeechBubbles from "../common/components/testimonials/WithSpeechBubbles";
+import ThreeTierPricing from "../common/components/pricing/ThreeTierPricing";
+import BasicStatistics from "../common/components/statistics/BasicStatistics";
+import StatisticsWithIcon from "../common/components/statistics/StatisticsWithIcon";
+import Carousel from "../common/components/carousels/Carousel";
+import WithSubNavigation from "../common/components/navbars/WithSubNavigation";
+import SidebarWithHeader from "../common/components/navbars/SidebarWithHeader";
+import Contact from "../common/components/contact/Contact";
 
 const Home: NextPage = () => {
   return (
